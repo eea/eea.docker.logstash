1167f552a7e101a18d2f465cbe1302ffa12eadf7
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2021-09-17 18:08:29 +0300

    updated entrypoint of logstash

--- docker_start.sh
+++ docker_start.sh
@@ -3,10 +3,14 @@
 if [[ -d "/custom_config" ]] && [[ "${DEV_ENV:-false}" != "true" ]]
 then
     echo "/custom_config exists on your filesystem."
-    rm /usr/share/logstash/pipeline/logstash.conf
-    ln -s /custom_config/logstash/logstash.conf /usr/share/logstash/pipeline
+    rm -r /usr/share/logstash/pipeline
+    ln -s /custom_config/logstash/pipeline /usr/share/logstash
+#    rm /usr/share/logstash/pipeline/logstash.conf
+#    ln -s /custom_config/logstash/logstash.conf /usr/share/logstash/pipeline
     rm /usr/share/logstash/config/logstash.yml
     ln -s /custom_config/logstash/logstash.yml /usr/share/logstash/config
+    rm /usr/share/logstash/config/pipelines.yml
+    ln -s /custom_config/logstash/pipelines.yml /usr/share/logstash/config
 fi
 
 /usr/local/bin/docker-entrypoint "$@"
